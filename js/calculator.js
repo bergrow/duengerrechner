29925659ca681e2c3f22e5e6104a588010c8506a
bergrow <bergrow@proton.me>
2025-01-24 10:32:37 +0100

    Separate table.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,13 +1,4 @@
 var systemTheme = () => (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-const convTable = {
-  cao: { to: "ca", factor: 0.715 },
-  k2o: { to: "k", factor: 0.8302 },
-  mgo: { to: "mg", factor: 0.603 },
-  no3: { to: "n", factor: 0.226 },
-  p2o5: { to: "p", factor: 0.436 },
-  po4: { to: "p", factor: 0.326 },
-  so4: { to: "s", factor: 0.334 },
-};
 var customFertData;
 var waterData;
 
@@ -311,44 +302,6 @@ const resetCalculator = (event) => {
   location.reload();
 };
 
-// Table functions
-const addRow = (tbody, eventHandler, appendix = null) => {
-  if (appendix) appendix.parentNode.removeChild(appendix);
-  let newRow = tbody.querySelector("tr:last-of-type").cloneNode(true);
-  clearRow(newRow);
-  if (eventHandler) newRow.addEventListener("input", eventHandler);
-  tbody.appendChild(newRow);
-  if (appendix) tbody.querySelector("tr:last-of-type").appendChild(appendix);
-  return newRow;
-};
-
-const updateRow = (row, data, multiplier = 1) => {
-  row.querySelectorAll("span").forEach((span) => {
-    const name = span.getAttribute("name");
-    let value = data[name];
-    value = isNaN(value) ? value : round(value * multiplier, 3);
-    span.dataset.value = value;
-    span.textContent = isNaN(value) ? value : formatValue(value, decimals(name));
-  });
-};
-
-const removeRow = (tbody, appendix = null) => {
-  tbody.removeChild(tbody.querySelector("tr:last-of-type"));
-  if (appendix) tbody.querySelector("tr:last-of-type").appendChild(appendix);
-};
-
-const clearRow = (row) => {
-  row.querySelectorAll("select").forEach((select) => (select.selectedIndex = 0));
-  row.querySelectorAll("input[type=number]").forEach((input) => (input.value = ""));
-  row.querySelectorAll("input[type=checkbox]").forEach((input) => (input.checked = false));
-  row.querySelectorAll("span").forEach((span) => {
-    span.textContent = "";
-    span.classList.remove("checked-off");
-    if ("value" in span.dataset) span.dataset.value = 0;
-    if ("original" in span.dataset) span.dataset.original = "";
-  });
-};
-
 // Helper functions
 const delimiter = () => {
   const delimiter = new Option("———", "-1");
@@ -364,3 +317,12 @@ const round = (v, p = 2) => toFloat(v.toFixed(p));
 const toFloat = (str, def = 0) => parseFloat(str) || def;
 const calcGH = (ca, mg) => ca * 0.14 + mg * 0.2307;
 const calcKH = (ks) => (ks === 0 ? 0 : (ks - 0.05) * 2.8);
+const convTable = {
+  cao: { to: "ca", factor: 0.715 },
+  k2o: { to: "k", factor: 0.8302 },
+  mgo: { to: "mg", factor: 0.603 },
+  no3: { to: "n", factor: 0.226 },
+  p2o5: { to: "p", factor: 0.436 },
+  po4: { to: "p", factor: 0.326 },
+  so4: { to: "s", factor: 0.334 },
+};
